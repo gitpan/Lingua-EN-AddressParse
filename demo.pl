756a32e91282bb1e2059f0f007bddca403166eec
Kim Ryan <kimryan@cpan.org>
1999-12-28 01:03:43 +0000

    Import of KIMRYAN/Lingua-EN-AddressParse-0.01 from CPAN.
    gitpan-cpan-distribution: Lingua-EN-AddressParse
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         KIMRYAN/Lingua-EN-AddressParse-0.01.tar.gz
    gitpan-cpan-author:       KIMRYAN
    gitpan-cpan-maturity:     released

---
+++ demo.pl
@@ -0,0 +1,74 @@
+#! /usr/local/bin/perl
+# Demo script for Lingua::EN::AddressParse.pm
+
+use Lingua::EN::AddressParse;
+
+
+my %args = 
+(
+   auto_clean  => 0,
+   force_case  => 1
+);
+
+
+my $address = new Lingua::EN::AddressParse(%args);
+open(OUT_FH,">report.txt") or die;
+open(ERROR_FH,">error.txt") or die;
+
+while ( <DATA> )
+{
+   chomp($_);
+   $address_in = $_; 
+   
+	$total++;
+	$error = $address->parse($address_in);
+   $error and $errors++; 
+
+	%comps = $address->case_components;
+	%props = $address->properties;
+   
+   if ( $error )
+   {
+		print(ERROR_FH $props{non_matching},"\n");
+   }
+   
+	elsif ( $props{type} eq "suburban" )
+	{
+	 	$line = sprintf("%-11.11s %-20.20s %-20.20s %-3.3s %4d %-10.10s\n",
+	   	$comps{property_identifier},$comps{street},$comps{suburb},
+	      $comps{state},$comps{post_code},$comps{country});
+		print(OUT_FH $line);
+	}
+   elsif ( $props{type} eq "rural" )
+   {
+	 	$line = sprintf("%-11.11s %-20.20s %-20.20s %-3.3s %4d %-10.10s\n",
+	   	"",$comps{property_name},$comps{suburb},$comps{state},$comps{post_code},$comps{country});
+		print(OUT_FH $line);
+   }
+   elsif ( $props{type} eq "post_box" )
+   {
+	 	$line = sprintf("%-11.11s %-20.20s %-20.20s %-3.3s %4d %-10.10s\n",
+	   	"",$comps{post_box},$comps{suburb},$comps{state},$comps{post_code},$comps{country});
+		print(OUT_FH $line);
+   }
+}
+close(ERROR_FH);
+close(OUT_FH);
+printf("BATCH DATA QUALITY: %5.2f percent\n",( 1- ($errors / $total)) *100 );
+
+#------------------------------------------------------------------------------
+__DATA__
+"OLD REGRET" WENTWORTH FALLS NSW 2782
+14A WANDARRA CRESCENT ST JOHNS WOOD SW 200
+147 CHARLESTOWN ROAD KOTARA HEIGHTS NSW 2289 AUSTRALIA
+Level 2/12 LEIGH CIRCUIT WERRIBEE HILLS VIC 3030
+2/3-5 AUBREY ST VERMONT VIC 3133
+60 Watkins Road Baulkham Hills NSW 2153
+74 KITCHENER LANE ST IVES NSW 2075
+UNIT 1/61 PANTEOWARA STREET CORLETTE NSW 2315
+26 george street holmesville nsw 2286
+RMS 75 HASSALL GROVE NSW 2761
+PO BOX 71 TOONGABBIE NSW 2146
+BAD ADDRESS GARDWELL 4849
+LOT 2C ARTHUR STREET CARDIFF NSW 2285
+12 SMITH ST ULTIMO NSW 2007 : ALL POSTAL DELIVERIES
---
+++ t/main.t
@@ -0,0 +1,56 @@
+#------------------------------------------------------------------------------
+# Test script for Lingua::EN::::AddressParse.pm 
+#                                            
+# Author: Kim Ryan (kimaryan@ozemail.com.au) 
+# Date  : 10 January 2000                         
+#------------------------------------------------------------------------------
+
+use strict;
+use Lingua::EN::AddressParse;
+
+# We start with some black magic to print on failure.
+
+BEGIN { print "1..2\n"; }
+
+# Main tests
+
+my $input;
+
+  my %args = 
+(
+  auto_clean  => 0,
+  force_case  => 1
+);
+
+my $address = new Lingua::EN::AddressParse(%args); 
+
+
+# Test component extraction
+$input = "12A/74-76 AMINTA CRESCENT HASALL GROVE NSW 2761 AUSTRALIA";
+$address->parse($input);
+my %comps = $address->case_components;
+if 
+( 
+   $comps{property_identifier} eq '12A/74-76' and 
+   $comps{street} eq 'Aminta Crescent' and 
+   $comps{suburb} eq 'Hasall Grove' and
+   $comps{state} eq 'NSW' and
+   $comps{post_code} eq '2761' and
+   $comps{country} eq 'AUSTRALIA'
+)
+{
+   print "ok 1\n";   
+}
+else
+{
+   print "not ok 1\n";  
+}
+
+
+# Test non matching 
+$input = "12 SMITH ST ULTIMO NSW 2007 : ALL POSTAL DELIVERIES";
+$address->parse($input);
+my %props = $address->properties;
+print $props{non_matching} eq ": ALL POSTAL DELIVERIES" ? "ok 2\n" : "not ok 2\n";
+      
+
